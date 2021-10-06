d306a3231c43096df7a89c92840ba9061dd54865
dk630 <dkuang@bwh.harvard.edu>
2021-10-06 10:08:01 -0400

    Add project files.

---
+++ MockRobot_API/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MockRobot_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static void MockRock_Server()
+        {
+            TcpListener server = null;
+
+            try
+            {
+                // create tcp network connection setting   
+                Int32 port = 1000;
+                IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+
+                // create new tcp listener with local address and port at 1000
+                server = new TcpListener(localAddress, port);
+
+                //
+
+                
+            } 
+            catch
+            {
+
+            }
+        }   
+    }
+}
---
+++ MockRobot_DDI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MockRobot_DDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
